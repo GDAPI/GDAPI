cf4cd8f720878f440dbc9a99ba2f2e0c25f53e41
Quasar-Kim <vhfflrhs128@gmail.com>
2022-07-09 23:49:13 +0900

    [crypto] Support test on node environment

--- packages/crypto/test/util/loadResource.js
+++ packages/crypto/test/util/loadResource.js
@@ -1,14 +1,42 @@
-export default async function loadResource (fileOrFiles) {
-  const files = []
+// load file(s) as Uint8Array in browser environment
+async function loadTextBrowser (fileOrFiles) {
   if (typeof fileOrFiles === 'string') {
     const res = await fetch(`/base/test/resource/${fileOrFiles}`)
     return new Uint8Array(await res.arrayBuffer())
   }
 
+  const files = []
   const results = []
   for (const file of files) {
     const res = await fetch(`/base/test/resource/${file}`)
     results.push(new Uint8Array(await res.arrayBuffer()))
   }
   return results
+}
+
+// load file(s) as Buffer(which is compatible with Uint8Array interface) in node environment
+async function loadTextNode (fileOrFiles) {
+  const { readFile } = await import('node:fs/promises')
+
+  if (typeof fileOrFiles === 'string') {
+    return await readFile(`test/resource/${fileOrFiles}`)
+  }
+
+  const files = []
+  const results = []
+  for (const file of files) {
+    const buffer = await readFile(`test/resource/${file}`)
+    results.push(buffer)
+  }
+  return results
+}
+
+export default async function loadResource (fileOrFiles) {
+  if (globalThis?.process?.env?.NODE_ENV === 'test') {
+    // node environment
+    return await loadTextNode(fileOrFiles)
+  } else {
+    // browser environment
+    return await loadTextBrowser(fileOrFiles)
+  }
 }
--- packages/crypto/test/util/loadText.js
+++ packages/crypto/test/util/loadText.js
@@ -1,14 +1,42 @@
-export default async function loadText (fileOrFiles) {
-  const files = []
+// load file(s) as text in browser environment
+async function loadTextBrowser (fileOrFiles) {
   if (typeof fileOrFiles === 'string') {
     const res = await fetch(`/base/test/resource/${fileOrFiles}`)
     return res.text()
   }
 
+  const files = []
   const results = []
   for (const file of files) {
     const res = await fetch(`/base/test/resource/${file}`)
     results.push(await res.text())
   }
   return results
+}
+
+// load file(s) as text in node environment
+async function loadTextNode (fileOrFiles) {
+  const { readFile } = await import('node:fs/promises')
+
+  if (typeof fileOrFiles === 'string') {
+    return await readFile(`test/resource/${fileOrFiles}`, { encoding: 'utf-8' })
+  }
+
+  const files = []
+  const results = []
+  for (const file of files) {
+    const text = await readFile(`test/resource/${file}`, { encoding: 'utf-8' })
+    results.push(text)
+  }
+  return results
+}
+
+export default async function loadText (fileOrFiles) {
+  if (globalThis?.process?.env?.NODE_ENV === 'test') {
+    // node environment
+    return await loadTextNode(fileOrFiles)
+  } else {
+    // browser environment
+    return await loadTextBrowser(fileOrFiles)
+  }
 }
